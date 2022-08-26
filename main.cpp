6309fc2d63f7108f363f6c8d1aa81a42a534e8a1
4EFI <49026937+4EFI@users.noreply.github.com>
2022-08-26 13:22:07 +0300

    GetFileStrs

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@
 #include "StringProcessing.h"
 #include "LOG/Log.h"
 
+//-----------------------------------------------------------------------------
+
+struct StrParams
+{
+    char* str = NULL;
+    int   len = 0;
+};
+
+//-----------------------------------------------------------------------------
+
+int GetFileStrs (FILE* file, StrParams arrStrs[]);
+
+//-----------------------------------------------------------------------------
+
 int main()
 {
     const char* unitTests[] = {"Vlados\n",
@@ -24,6 +38,24 @@ int main()
         printf ("An error during opening file...\n");
     }
 
+    StrParams* arrStrs = NULL;
+    int numStrs = GetFileStrs (file, arrStrs);
+
+    fclose (file);
+
+    return 0;
+}
+
+//-----------------------------------------------------------------------------
+
+int GetFileStrs (FILE* file, StrParams arrStrs[])
+{
+    $LOG_LVL_UP
+
+    //{ ASSERT
+    assert (file != NULL);
+    //}
+
     fseek (file, 0, SEEK_END);
 
     int fileSize = ftell (file);
@@ -32,27 +64,54 @@ int main()
 
     char* str = (char*) calloc (sizeof (char), fileSize + 1);
 
-    for (int i = 0; i < fileSize; i++)
-    {
-        FLOG ("%d", str[i]);
-    }
-
     fseek (file, 0, SEEK_SET);
 
     int rightRead = fread (str, sizeof (char), fileSize, file);
 
-    FLOG ("right read = %d", rightRead);
-
+    FLOG ("right read = %d",          rightRead);
     FLOG ("last str sym = '%c' = %d", str[fileSize], str[fileSize]);
+    FLOG ("sizeof char ** = %d",      sizeof(char**));
 
-    printf ("'%s'", str);
+    int numStrs = 0;
 
     for (int i = 0; i < fileSize; i++)
     {
-
+        if (str[i] == '\n')
+        {
+            numStrs++;
+        }
     }
 
-    fclose (file);
+    arrStrs = (StrParams*) calloc (sizeof (StrParams), numStrs);
 
-    return 0;
+    int pos = 0, nowStr = 0;
+
+    for (int i = 0; i < fileSize; i++)
+    {
+        if (str[i] == '\n')
+        {
+            arrStrs[nowStr].str = &str[i - pos];
+            //+1 for '\0'
+            arrStrs[nowStr].len      = pos + 1;
+            arrStrs[nowStr].str[pos]     = '\0';
+            arrStrs[nowStr].str[pos - 1] = '\0';
+
+            pos = 0;
+            nowStr++;
+
+            continue;
+        }
+
+        pos++;
+    }
+
+    for (int i = 0; i < numStrs; i++)
+    {
+        LOG ("%d) '%s'", i + 1, arrStrs[i].str);
+    }
+
+    return numStrs;
 }
+
+//-----------------------------------------------------------------------------
+
